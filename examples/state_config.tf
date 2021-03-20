516502bad7a7a7b2e951b54a0231d2d8088c3083
Jose Dalino <jrdalino@gmail.com>
2021-03-21 01:58:37 +0800

    Initial

--- examples/state_config.tf
+++ examples/state_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "ap-southeast-1"
     bucket         = "623552185285-ap-southeast-1-terraform-state" // AWS acccount where state backend is located
-    key            = "aws-ecr-terraform.tfstate"
+    key            = "bbsdm-organisation-aws-ecr-terraform.tfstate"
     encrypt        = "true"
     dynamodb_table = "terraform-state-lock"
   }
